ad2b50bcf96761d26948c87a5ed4a6ae43aef03c
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-05-07 10:58:30 +0800

    initial commit

---
+++ app/src/main/java/com/example/smartfeeder/FeederActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartfeeder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeederActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feeder)
+    }
+}
