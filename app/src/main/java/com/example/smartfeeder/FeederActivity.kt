cbb4adb091159c3ed407c5937e6cbc9aa22cd598
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-05-12 12:09:32 +0800

    okhttp implementation

--- app/src/main/java/com/example/smartfeeder/FeederActivity.kt
+++ app/src/main/java/com/example/smartfeeder/FeederActivity.kt
@@ -1,27 +1,34 @@
 package com.example.smartfeeder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
+import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.smartfeeder.databinding.ActivityFeederBinding
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.*
+import java.io.IOException
 
 class FeederActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeederBinding
-
+    private val client = OkHttpClient()
     private var ready = false
+    private var datr = ""
+    private var ip = ""
+    private var port = ""
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feeder)
+        datr = intent.getStringExtra("datr").toString()
+        ip = intent.getStringExtra("ip").toString()
+        port = intent.getStringExtra("port").toString()
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_feeder)
         binding.lora = Lora("-.---", "-.---", "", "01/01/1970 00:00")
@@ -34,19 +41,42 @@ class FeederActivity : AppCompatActivity() {
         val buttonApply = findViewById<Button>(R.id.apply)
 
         buttonDispense.setOnClickListener {
-            Toast.makeText(baseContext, "Won\'t dispense \'til data is ready",
-                Toast.LENGTH_SHORT).show()
+            if (!ready) {
+                Toast.makeText(
+                    baseContext, baseContext.getString(R.string.data_not_ready), Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            // TODO: action on ready
         }
         buttonApply.setOnClickListener {
-            Toast.makeText(baseContext, "Won\'t apply \'til data is ready",
-                Toast.LENGTH_SHORT).show()
+            if (!ready) {
+                Toast.makeText(
+                    baseContext, baseContext.getString(R.string.data_not_ready), Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            // TODO: action on ready
         }
-
-        val editTextRate = findViewById<EditText>(R.id.rate_second)
-        editTextRate.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
     }
 
     private fun getData() {
-        
+        val formBody = FormBody.Builder().add("datr", datr).build()
+        val request = Request.Builder().url("http://$ip:$port/status").post(formBody).build()
+        Log.d("d/response", "Connecting to: http://$ip:$port/status")
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(applicationContext, applicationContext.getString(
+                        R.string.no_connection_to_server), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("d/response", response.body.toString())
+            }
+        })
     }
 }
--- app/src/main/java/com/example/smartfeeder/MainActivity.kt
+++ app/src/main/java/com/example/smartfeeder/MainActivity.kt
@@ -4,15 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val etDatr = findViewById<EditText>(R.id.datr)
+        val etIp = findViewById<EditText>(R.id.ip)
+        val etPort = findViewById<EditText>(R.id.port)
         val button = findViewById<Button>(R.id.toFeeder)
+
         button.setOnClickListener {
             val intent = Intent(this, FeederActivity::class.java)
+            intent.putExtra("datr", etDatr.text.toString())
+            intent.putExtra("ip", etIp.text.toString())
+            intent.putExtra("port", etPort.text.toString())
             startActivity(intent)
         }
     }
